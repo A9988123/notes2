9fcbe4dbf1ca14bb4651832a20deda23bcea9346
A9988123 <99227239+A9988123@users.noreply.github.com>
2026-01-04 16:13:06 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "notes-pwa-v1";
+const FILES = [
+  "./notes2.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
